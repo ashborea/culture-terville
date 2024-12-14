95425defc6f2c00d9bdf60355465ff8ff88f4a87
Marjorie Siad <ashborea@gmail.com>
2024-12-14 22:41:13 +0100

    add .env in gitignore, oopsie

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div className='bg-green-600'>Navbar</div>
+    <div className='bg-yellow-500 h-12'>
+        <div>
+            <Link to='/'>Culture <span className='text-main-red'>Terville</span></Link>
+            
+        </div>
+    </div>
   )
 }
 
